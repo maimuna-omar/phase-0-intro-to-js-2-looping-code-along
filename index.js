e91d7130e813fc8be24d7a0c6a2ea91bab08fe72
maimuna omar <maymunaabdi07@gmail.com>
2023-03-25 15:43:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const names = (["Guadalupe", "Ollie", "Aki"]);
+
+function writeCards(names) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+       // console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        debugger;
+    }
+        return thankYouMessages;
+}
+writeCards(names);
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+countDown(4);
